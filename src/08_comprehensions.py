211c11b9a11290b6905a0d8b65f95c53ae977729
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2020-02-13 16:17:50 -0800

    comprehension done

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -11,14 +11,16 @@ for more info regarding list comprehensions.
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
 y = []
-
+for value in range(5):
+    y.append(value + 1)
 print (y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
 y = []
-
+for value in range(10):
+    y.append(value**3)
 print(y)
 
 # Write a list comprehension to produce the uppercase version of all the
@@ -26,8 +28,7 @@ print(y)
 
 a = ["foo", "bar", "baz"]
 
-y = []
-
+y = [x.upper() for x in a]
 print(y)
 
 # Use a list comprehension to create a list containing only the _even_ elements
@@ -37,5 +38,8 @@ x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
 y = []
-
+ 
+""" int type conversion is used because when you use input, the result is a string. 
+    In order to do arithmetic on the input value, you need to convert it to integer or float """
+y = [i for i in x if int(i) % 2 == 0]
 print(y)
