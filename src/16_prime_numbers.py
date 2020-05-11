4a1ba0f93b5043ddd695ca15e44dab46733953ea
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2020-05-11 13:51:00 -0700

    time added to stretch

--- src/16_prime_numbers.py
+++ src/16_prime_numbers.py
@@ -2,8 +2,8 @@ import time
 
 def prime_number(n):
     '''if number is prime, return true, otherwise false'''
-    for divisor in range(2, n):
-        if n % 2 == 0:
+    for d in range(2, n):
+        if n % d == 0:
             return False
     return True
 
@@ -11,3 +11,10 @@ def prime_number(n):
 ''' TEST '''
 for n in range(1, 50):
     print(n, prime_number(n))
+
+''' TIME '''
+t0 = time.time()
+for n in range(1, 10000):
+    prime_number(n)
+t1 = time.time()
+print("Completed in: ", t1 - t0)
